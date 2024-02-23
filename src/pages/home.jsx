7378d8ad3d70bf309bcaa6223dc081ed734a675c
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2024-02-22 23:49:19 -0330

    netlify form x2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -167,8 +167,38 @@ export function Home() {
             Complete this form and we will get back to you in 24 hours.
           </PageTitle>
 
+
+          {/* <!-- dummy so netlify knows what's coming -->
+                    <div class="container">
+                      <h1 class="title">
+                        Contact
+                      </h1>
+                      <div class="content">
+                        
+                    <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+                          <input type="hidden" name="form-name" value="contact" />
+                          <p class="hidden">     
+                            <label>Don’t fill this out: <input name="bot-field"></label>   
+                          </p>
+                          <label class="form-label" for="name">
+                            Name:
+                          </label>
+                          <input class="form-field" name="name" id="name" />
+                          <label class="form-label" for="email">
+                            Email:
+                          </label>
+                          <input class="form-field" name="email" id="email" />
+                          <label class="form-label" for="message">
+                            Message:
+                          </label>
+                          <textarea class="form-field" name="message" id="message"></textarea>
+                          <input class="form-button" type="submit" value="Send message" />
+                        </form>
+                      </div>
+                    </div>  */}
+
           
-          <form name="contact" netlify className="mx-auto w-full mt-12 lg:w-5/12">
+          <form name="contact" className="mx-auto w-full mt-12 lg:w-5/12" netlify>
             <div className="mb-8 flex gap-8">
               <Input variant="outlined" size="lg" label="Full Name" />
               <Input variant="outlined" size="lg" label="Email Address" />
